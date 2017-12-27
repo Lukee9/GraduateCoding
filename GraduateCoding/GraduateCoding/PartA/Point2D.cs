6a91d04e040189283b44cfa9874282728519e326
Lukee9 <wowman999@live.co.uk>
2017-12-27 18:34:34 +0000

    Added various helpful comments and added initial code to sort array of Point2D objects. Also added code to start checking that methods we are creating work as expected.

--- GraduateCoding/GraduateCoding/PartA/Point2D.cs
+++ GraduateCoding/GraduateCoding/PartA/Point2D.cs
@@ -13,7 +13,20 @@ namespace GraduateCoding.PartA
 
         public static double calculateDistance(Point2D pointOne, Point2D pointTwo)
         {
+            //Returns the square root of the difference of the x values squared and the y value difference squared
             return Math.Sqrt(Math.Pow(pointTwo.x - pointOne.x, 2) + Math.Pow(pointTwo.y - pointOne.y, 2));
+        }
+        public static Point2D[] sort(Point2D[] Points)
+        {
+            //Defines the origin as (0,0)
+            Point2D origin = new Point2D();
+            origin.x = 0;
+            origin.y = 0;
+            //Converts the array of points to a list so it can be sorted using an anonymous function based on the calculateDistance method
+            Points.ToList().Sort((a, b) => (
+            calculateDistance(a, origin).CompareTo(calculateDistance(b, origin)))
+            );
+            return Points;
         }
     }
 }
--- GraduateCoding/GraduateCoding/Program.cs
+++ GraduateCoding/GraduateCoding/Program.cs
@@ -10,7 +10,18 @@ namespace GraduateCoding
     {
         static void Main(string[] args)
         {
-            
+            //Testing Part A Question 1:
+            PartA.Point2D pointOne = new PartA.Point2D();
+            PartA.Point2D pointTwo = new PartA.Point2D();
+            pointOne.x = 5;
+            pointOne.y = 3;
+            pointTwo.x = 6;
+            pointTwo.y = 9;
+            //Checking results
+            Console.WriteLine(PartA.Point2D.calculateDistance(pointOne, pointTwo));
+
+
+            Console.ReadLine();
         }
     }
 }
