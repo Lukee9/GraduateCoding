6eb1040c272c5e3b9c05270281fb411918917917
Lukee9 <wowman999@live.co.uk>
2017-12-27 18:14:22 +0000

    Added code to calculate distance between two points

---
+++ GraduateCoding/GraduateCoding/PartA/Point2D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduateCoding.PartA
+{
+    public class Point2D
+    {
+        public int x;
+        public int y;
+
+        public static double calculateDistance(Point2D pointOne, Point2D pointTwo)
+        {
+            return Math.Sqrt(Math.Pow(pointTwo.x - pointOne.x, 2) + Math.Pow(pointTwo.y - pointOne.y, 2));
+        }
+    }
+}
--- GraduateCoding/GraduateCoding/Program.cs
+++ GraduateCoding/GraduateCoding/Program.cs
@@ -10,6 +10,7 @@ namespace GraduateCoding
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
