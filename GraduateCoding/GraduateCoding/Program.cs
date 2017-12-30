0dd49cda9a2bc7fb24fe61a308c3bfd8f625ec17
Lukee9 <wowman999@live.co.uk>
2017-12-30 17:23:09 +0000

    Added code to test collinear and anticlockwise methods, also added code to make test results easier to read on console

--- GraduateCoding/GraduateCoding/Program.cs
+++ GraduateCoding/GraduateCoding/Program.cs
@@ -11,6 +11,7 @@ namespace GraduateCoding
         static void Main(string[] args)
         {
             //Testing Part A Question 1:
+            Console.WriteLine("Part A Question 1:");
             PartA.Point2D pointOne = new PartA.Point2D();
             PartA.Point2D pointTwo = new PartA.Point2D();
             pointOne.x = 5;
@@ -21,6 +22,7 @@ namespace GraduateCoding
             Console.WriteLine(PartA.Point2D.calculateDistance(pointOne, pointTwo));
 
             //Testing Part A Question 2:
+            Console.WriteLine(Environment.NewLine + "Part A Question 2:");
             PartA.Point2D pointThree = new PartA.Point2D();
             PartA.Point2D pointFour = new PartA.Point2D();
             PartA.Point2D pointFive = new PartA.Point2D();
@@ -38,6 +40,26 @@ namespace GraduateCoding
             {
                 Console.WriteLine($"({p.x}, {p.y})");
             }
+
+            //Testing Part A Question 3:
+            Console.WriteLine(Environment.NewLine + "Part A Question 3:");
+            PartA.Point2D pointSix = new PartA.Point2D();
+            PartA.Point2D pointSeven = new PartA.Point2D();
+            pointSix.x = 2;
+            pointSix.y = 1;
+            pointSeven.x = 3;
+            pointSeven.y = 2;
+
+            //Checking results
+            //Expecting true
+            Console.WriteLine(PartA.Point2D.isAntiClockwise(pointSix, pointSeven, pointThree));
+            //Expecting false
+            Console.WriteLine(PartA.Point2D.isAntiClockwise(pointFour, pointSeven, pointThree));
+            //Expecting true
+            Console.WriteLine(PartA.Point2D.isCollinear(pointThree, pointThree, pointSeven));
+            //Expecting false
+            Console.WriteLine(PartA.Point2D.isCollinear(pointFour, pointSeven, pointThree));
+
             Console.ReadLine();
         }
     }
